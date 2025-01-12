4576d371046b302e1366d38241fc4cb4c409f2c0
caioodym <157076808+caioodym@users.noreply.github.com>
2025-01-11 22:18:51 -0300

    Currículo em forma de site

---
+++ src/comandos.js
@@ -0,0 +1,21 @@
+var botum = document.getElementById('botsobrue').innerHTML;
+var botdois = document.getElementById('botform').innerHTML;
+var bottres = document.getElementById('botcont').innerHTML;
+var botquatro = document.getElementById('botexp').innerHTML;
+
+function ativarSobre() {
+    console.log(`Você clicou no botão ${botum}.`)
+}
+
+function ativarForms() {
+    console.log(`Você clicou no botão ${botdois}.`)
+}
+
+function ativarCont() {
+    console.log(`Você clicou no botão ${bottres}.`)
+}
+
+function ativarExp()  {
+    console.log(`Você clicou no botão ${botquatro}.`)
+}
+
